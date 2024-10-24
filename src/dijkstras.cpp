119816461cbf095b23092f0ca309d4b337ef5850
tkd995 <tkd995@vols.utk.edu>
2024-10-23 22:58:04 -0400

    final, fuck it

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -14,8 +14,8 @@ using namespace std;
 struct maze{
     map<char, int> costs;
     vector<int> tiles;
-    int start, end;
-    int row, col;
+    int start, end;//goal and beginning
+    int row, col;//size of matrix
     vector<signed int> visited;
 
     vector<int> path;
@@ -24,7 +24,7 @@ struct maze{
 
 void generate_map(int n){
 
-    default_random_engine gen;
+    default_random_engine gen;//random number generators for new game creation
     uniform_int_distribution<int> dista(97, 122);
     uniform_int_distribution<int> distb(1, n+1);
     uniform_int_distribution<int> distc(0, n);
@@ -32,7 +32,7 @@ void generate_map(int n){
     cout << n/2 << endl;
     vector<int> g(n, 0);
     for(int i = 0; i < n/2; i++){
-        int choc = dista(gen);
+        int choc = dista(gen);//random letters
         if(find(g.begin(), g.end(), choc) == g.end()){
             cout << i << " " << distb(gen) << endl;
             g[i] = choc;
@@ -60,13 +60,13 @@ void generate_map(int n){
 void dikstras(maze &m){
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     m.visited[m.start] = 0;
-    pq.emplace(0, m.start);
+    pq.emplace(0, m.start);//start
 
     while(!pq.empty()){
         int distance = pq.top().first, position = pq.top().second;
-        pq.pop();
+        pq.pop();//remove searched
 
-        if(position == m.end){
+        if(position == m.end){//if successful
             m.path_length = distance;
             return;
         }
@@ -75,24 +75,35 @@ void dikstras(maze &m){
             continue;
         }
 
-        vector<int> directions = {-1, 1, -m.col, m.col};
-        for (int dir : directions) {
-            int neighbor = position + dir;
-                    // Check bounds
-            if (neighbor >= 0 && neighbor < m.row * m.col) {
-                // Calculate new distance
-                int new_distance = distance + m.tiles[neighbor];
+        vector<int> directions;
+        
+        if(position%m.col != m.col-1){
+            directions.push_back(1);
+        }
+        if(position/m.col != m.row-1){
+            directions.push_back(m.col);
+        }
+        if(position/m.col != 0){
+            directions.push_back(-m.col);
+        }
+        if(position%m.col != 0){
+            directions.push_back(-1);
+        }
 
-                // If this new distance is better, update and push to queue
+        for (int dir : directions) {//check each direction
+            int neighbor = position + dir;
+
+            if (neighbor >= 0 && neighbor < m.row * m.col) {
+                int new_distance = distance + m.tiles[neighbor];//distance of new path
+
                 if (m.visited[neighbor] == -1 || new_distance < m.visited[neighbor]) {
                     m.visited[neighbor] = new_distance;
                     pq.emplace(new_distance, neighbor);
-                    m.path[neighbor] = position; // Track the path
+                    m.path[neighbor] = position;//add to traversed
                 }
             }
         }
     }
-    m.path_length = m.visited[m.end];
 }
 
 void store_map(maze &m){
@@ -104,30 +115,30 @@ void store_map(maze &m){
         int b;
         cin >> a >> b;
 
-        m.costs.emplace(make_pair(a, b));
+        m.costs.emplace(make_pair(a, b));//add to dict
     }
 
     int row, col;
-    cin >> row >> col;
+    cin >> row >> col;//init based on size
     m.tiles = vector<int>(col*row, 0);
-    m.path = vector<int>(col*row, 0);
+    m.path = vector<int>(col*row, -1);
 
     for(int k = 0; k < col; k++){
         for(int j = 0; j < row; j++){
             char c;
             cin >> c;
 
-            m.tiles[j*col+k] = m.costs[c];
+            m.tiles[j*col+k] = m.costs[c];//value of tile
         }
     }
-    m.visited = vector<signed int>(row*col, -1);
+    m.visited = vector<signed int>(row*col, -1);//init
 
     int tx, ty;
     cin >> tx >> ty;
-    m.start = tx*col+ty;
+    m.start = ty*col+tx;
 
     cin >> tx >> ty;
-    m.end = tx*col+ty;
+    m.end = ty*col+tx;
 
     m.row = row;
     m.col = col;
@@ -144,12 +155,12 @@ int main(int argc, char *argv[]) {
     cout<<(m->path_length)<<endl;
     vector<string> s;
 
-    for(int i = m->end; i != 0; i = m->path[i]){
+    for(int i = m->end; i != -1; i = m->path[i]){
 	   s.push_back(to_string(i%m->col) + " " + to_string(i/m->col));
     }
-    s.push_back("0 0");
+
     reverse(s.begin(), s.end());
-    reverse(s[s.size()-1].begin(), s[s.size()-1].end());
+    //reverse(s[s.size()-1].begin(), s[s.size()-1].end());
 
     for(auto j = s.begin(); j != s.end(); ++j){
         cout<<*j<<endl;
